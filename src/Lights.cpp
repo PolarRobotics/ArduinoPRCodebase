5dd3b22f5cb7d7a9968966737002d19060b51f88
rdavies02 <72978944+rdavies02@users.noreply.github.com>
2022-10-03 20:52:51 -0400

    working

--- src/Lights.cpp
+++ src/Lights.cpp
@@ -5,94 +5,126 @@
 #define LED_PIN 7
 #define NUM_LEDS 30 
 
-bool Green1 = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BE SOLID GREEN
-bool Green2 = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BLINK GREEN
-bool Blue = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BE SOLID BLUE
-bool Red = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BLINK RED
+// bool Green1 = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BE SOLID GREEN
+// bool Green2 = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BLINK GREEN
+// bool Blue = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BE SOLID BLUE
+// bool Red = false; // VARIABLE TO DETERMINE IF LIGHTS SHOULD BLINK RED
 
-
-CRGBArray<NUM_LEDS> leds;
-
-struct LEDS {
-  uint32_t m_color;
-  int m_state;
+enum LEDState {
+  PAIRING,      // flash Blue
+  PAIRED,       // green then fade out
+  OFFENSE,      // green
+  DEFENSE,      // blue
+  BALL_CARRIER, // flash red
 };
 
-void setup(){
-FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
-FastLED.setMaxPowerInVoltsAndMilliamps(5, 500); // Power Failsafe
-// Clears LEDs when code is updated
-FastLED.clear();
-FastLED.show();
+LEDState currState;
+CRGBArray<NUM_LEDS> leds;
+uint8_t iteration = 255;
+
+void setLEDStatus(LEDState status);
+void updateLEDS();
+
+void setup() {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 500); // Power Failsafe
+  // Clears LEDs when code is updated
+  FastLED.clear();
+  FastLED.show();
 }
 
 void loop(){
+  // Determining when 
 
+  /// Sets all leds to solid green when 'x'
+  // for(int i = 0; i < NUM_LEDS; i++){
+  //   while(Green1 == true){
+  //   leds[i] = CRGB(0, 255, 0);
+  //   FastLED.show();
+  // }
+  setLEDStatus(PAIRED);
+  updateLEDS();
+  FastLED.delay(20);
 
-// Determining when 
-if(...){
-  Green1 = true;
+  iteration += 15;
 }
 
-if(...){
-  Green2 = true; 
+void setLEDStatus(LEDState status) {
+  currState = status;
 }
 
-if(...){
-  Blue = true;
-}
-
-if(...){
-  Red = true;
-}
-
-  FastLED.setBrightness(0);
-}
-
-/// Sets all leds to solid green when 'x'
-for(int i = 0; i < NUM_LEDS; i++){
-  while(Green1 == true){
-  leds[i] = CRGB(0, 255, 0);
-  FastLED.show();
+void updateLEDS() {
+  switch(currState) {
+    case PAIRING: {
+      break;
+    }
+    case PAIRED: {
+      leds = CRGB::Green;
+      FastLED.setBrightness(iteration);
+      break;
+    }
+    case OFFENSE: {
+      leds = CRGB::Blue;
+      break;
+    }
+    case DEFENSE: {
+      leds = CRGB::Green;
+      break;
+    }
+    case BALL_CARRIER: {
+      if(iteration % 255/16 == 0) {
+        leds = CRGB::Red;
+        FastLED.delay(30);
+      } else {
+        leds = CRGB::Black;
+      }
+      break;
+    }
+    default: {
+      leds = CRGB::Red;
+      break;
+    }
   }
+  FastLED.show();
 }
 
 
-// Turn lights Flashing Green
-for(int i = 0; i < NUM_LEDS; i++){ // Flash on
-  while(Green2 == true){
-  leds[i] = CRGB(0, 255, 0);
-  FastLED.setBrightness(2*i);
-  FastLED.show();
-  delay(100);
-  }
-}
-for(int i = NUM_LEDS; i > 0; i--){ // Flash off
-  while(Green2 == true){
-  leds[i] = CRGB(0, 255, 0);
-  FastLED.setBrightness(60-2*i);
-  FastLED.show();
-  delay(100);
-  }
-}
 
-// Turn lights Flashing Red
-for(int i = 0; i < NUM_LEDS; i++){ // Flash on
-  while(Red == true){
-  leds[i] = CRGB(255, 0, 0);
-  FastLED.setBrightness(2*i);
-  FastLED.show();
-  delay(100);
-  }
-}
-for(int i = NUM_LEDS; i > 0; i--){ // Flash off
-  while(Red == true){
-  leds[i] = CRGB(255, 0, 0);
-  FastLED.setBrightness(60-2*i);
-  FastLED.show();
-  delay(100);
-  }
-}
+// // Turn lights Flashing Green
+// for(int i = 0; i < NUM_LEDS; i++){ // Flash on
+//   while(Green2 == true){
+//   leds[i] = CRGB(0, 255, 0);
+//   FastLED.setBrightness(2*i);
+//   FastLED.show();
+//   delay(100);
+//   }
+// }
+// for(int i = NUM_LEDS; i > 0; i--){ // Flash off
+//   while(Green2 == true){
+//   leds[i] = CRGB(0, 255, 0);
+//   FastLED.setBrightness(60-2*i);
+//   FastLED.show();
+//   delay(100);
+//   }
+// }
+
+// // Turn lights Flashing Red
+// for(int i = 0; i < NUM_LEDS; i++){ // Flash on
+//   while(Red == true){
+//   leds[i] = CRGB(255, 0, 0);
+//   FastLED.setBrightness(2*i);
+//   FastLED.show();
+//   delay(100);
+//   }
+// }
+// for(int i = NUM_LEDS; i > 0; i--){ // Flash off
+//   while(Red == true){
+//   leds[i] = CRGB(255, 0, 0);
+//   FastLED.setBrightness(60-2*i);
+//   FastLED.show();
+//   delay(100);
+//   }
+// }
 
 // // Sets all leds to solid green when 'x'
 // if(Green1 == true){ 
@@ -102,4 +134,3 @@ for(int i = NUM_LEDS; i > 0; i--){ // Flash off
 
 
 
-}
